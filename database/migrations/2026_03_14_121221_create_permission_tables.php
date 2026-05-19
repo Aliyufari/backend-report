a7265ec456ce15c0c88b142a2f942c211b04af9b
Aliyu Abubakar <aliyufari@gmail.com>
2026-05-19 13:56:57 +0100

    Added state,zone,lga,ward and pu  management

--- database/migrations/2026_03_14_121221_create_permission_tables.php
+++ database/migrations/2026_03_14_121221_create_permission_tables.php
@@ -54,7 +54,7 @@ return new class extends Migration
         Schema::create($tableNames['model_has_permissions'], static function (Blueprint $table) use ($tableNames, $columnNames, $pivotPermission, $teams) {
             $table->uuid($pivotPermission);
 
-            $table->string('model_type');
+            $table->string('model_type', 100);
             $table->uuid($columnNames['model_morph_key']);
             $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
 
